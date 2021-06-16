caf82ca87ffc5bead3c24cdd3b1a9f73ec7d109f
Frederik Koenig <frederik.koenig.work@gmail.com>
2021-06-16 14:52:30 +0200

    Requesting Script added.

---
+++ webrequest.py
@@ -0,0 +1,14 @@
+import requests 
+import time
+
+
+
+
+for i in range(1,100):
+
+    url = 'http://127.0.0.1:5000/calc/'+str(i)+'/'+str(i)+'' 
+    response = requests.get(url)        # To execute get request 
+    print(response.status_code)     # To print http response code  
+    print(response.text)            # To print formatted JSON response 
+
+    time.sleep(1)
