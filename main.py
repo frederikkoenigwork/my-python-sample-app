dd37058fad2948dc78b90ce190ff33faada4c342
Frederik Koenig <frederik.koenig.work@gmail.com>
2021-06-18 18:19:37 +0200

    Playing with the Lang.

---
+++ main.py
@@ -0,0 +1,33 @@
+import django
+print(django.get_version())
+
+
+print(f"boa {111 * 6}")
+
+
+print(f"{6*6}")
+
+
+input = input("Hey abuser, enter some stuff!\n")
+
+cmp = 1 > 0
+print(type(cmp))
+
+
+print(input)
+
+
+try:
+    int(asdf)
+except Exception:
+    print("Oh no, it failed!")
+
+
+while False:
+    print("False!")
+
+listeL = [1,2,3,45,6,4,3,6,8,6,3,3]
+
+listeL.append(33333)
+
+print(listeL)
