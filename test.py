11e89c3a078b0b1a185ebb4ee75aae3992adc21a
Frederik Koenig <frederik.koenig.work@gmail.com>
2021-06-18 13:27:19 +0200

    Playing with the Lang.

---
+++ test.py
@@ -0,0 +1,21 @@
+
+stuff = "Lorem Ipsum dolor si amat saflksajföldsakjfsadf"
+
+char = "a"
+
+liste = [1,2,3,4,5,6]
+
+listenliste = [[1, 2, 3], [1,2,3]]
+
+print(stuff)
+
+
+x = 42
+
+if x < 42:
+    print("Oh weh")
+elif x==42:
+    print("The answer to all questions.")
+else:
+    print("Bigger than 42.")
+
